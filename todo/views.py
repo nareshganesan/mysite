4ddb749f3924594160cb89385ebb030acc4dd9df
ng <nareshkumarganesan@gmail.com>
2015-06-09 02:29:07 -0700

    Todo detail view with back navigation.

--- todo/views.py
+++ todo/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
 
 from .models import Todo
 
 # Create your views here.
 
 
-def index(request):
+def todolist(request):
     latest_todo_list = Todo.objects.order_by('-priority')[:10]
     context =  {'latest_todo_list': latest_todo_list}
     return render(request, 'todo/index.html', context)
+
+def tododetail(request, todo_id):
+    try:
+        todo = Todo.objects.get(pk=todo_id)
+    except Todo.DoesNotExist:
+        raise Http404("Todo does not exist.")
+    return render(request, 'todo/detail.html', {'todo': todo})
