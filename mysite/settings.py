243e5e2549f5f2cceb7bf3515a8dc5b4a7c36ec0
Naresh <nareshkumarganesan@users.noreply.github.com>
2015-06-10 17:09:44 -0700

    Updating settings.py
    Adding root user for travis ci testing

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,8 +80,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mysite',
-	'USER': 'naresh',
-	'PASSWORD': 'naresh',
+	'USER': 'root',
+	'PASSWORD': '',
 	'HOST':'127.0.0.1',
 	'PORT': '3306',
     }
