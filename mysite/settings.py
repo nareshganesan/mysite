55727a69ed4ed952cc02ea3baafea14aa03957d1
ng <nareshkumarganesan@gmail.com>
2015-06-10 19:58:23 -0700

    Update setting.py - Travis user

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,11 +80,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mysite',
-	'USER': 'root',
-	'PASSWORD': '',
-	'HOST':'127.0.0.1',
-	'PORT': '3306',
-    }
+        'USER': 'travis',
+        'PASSWORD': '',
+        'HOST':'127.0.0.1',
+        'PORT': '3306',
+        }
 }
 
 
